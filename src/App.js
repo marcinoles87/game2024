19ad31891d014f11f5d5497f5474f86a762f342d
marcinoles87 <marcinoles87@gmail.com>
2025-08-06 15:05:46 +0200

    add new function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleMove = (e) =>{
+
+    
+    
+    // console.log(e.clientX)
+
+    // const playerX = e.clientX
+    // const playerY = e.clientY
+
+    // console.log(playerX , playerY)
+
+    
+  }
+
+  const player = document.querySelector('.player')
+  player.addEventListener('click' , handleMove )
+
+
+  
   return (
-    <div className="App">
-      
+    <div className="App" >
+     <div className="player"></div>
     </div>
   );
 }
