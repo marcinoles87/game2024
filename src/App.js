13eda13a4bd24c568d8ee919ba43f0bdddee4675
marcinoles87 <marcinoles87@gmail.com>
2025-08-07 15:15:01 +0200

    add new function

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [cordinatesX , setCordinatesX] = useState('')
+  const [cordinatesY , setCordinatesY] = useState('')
+
+  document.addEventListener( 'mousemove' , (e) =>{
+    setCordinatesX(e.clientX)
+    setCordinatesY(e.clientY)
+  })
+
   const change = (e) =>{
-   
+    
+    const player = document.querySelector('.player')
+
     console.log(e)
+
+    const x = e.clientX;
+    const y = e.clientY;
+    const move = 10;
+    const newX = x+move;
+    console.log(newX)
     player.style.backgroundColor = 'green'
+    player.movementX = `${newX}`
+
   }
 
     
-   const player = document.querySelector('.player')
-    player.addEventListener('click' , () => {
-      change()
-    })
+   
 
-
-
-    
-  
-
-  
-
-
-  
   return (
-    <div className="App" >
-     <div className="player"></div>
+    <div className="App" onClick={change}>
+     <div className="player" ></div>
+     <p>{cordinatesX}</p>
+     <p>{cordinatesY}</p>
     </div>
   );
 }
