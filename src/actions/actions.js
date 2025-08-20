6be02a2a9add984759b31dc1d1803e42b0863719
marcinoles87 <marcinoles87@gmail.com>
2025-08-20 19:12:50 +0200

    add new redux

---
+++ src/actions/actions.js
@@ -0,0 +1,14 @@
+export const ADD = 'ADD';
+export const DELETE = 'DELETE';
+export const EDIT = 'EDIT'
+
+
+{
+    type: ADD ,
+    payload : {
+        author : 'Jan Kowalski',
+        rate : 5 ,
+        comment : 'Super ksiazka' , 
+        
+    }
+}
